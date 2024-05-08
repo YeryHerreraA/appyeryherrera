88b2804d5d739b104730e19355d1f06042abc49d
YeryHerreraA <yery_herrera@hotmail.com>
2024-05-07 21:39:32 -0400

    referencias obtencion de valores y validaciones

--- app/src/main/java/com/example/appyeryherrera/MainActivity.kt
+++ app/src/main/java/com/example/appyeryherrera/MainActivity.kt
@@ -23,23 +23,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         //referencias a los widget
-        val btn_iniciar_sesion_login = findViewById<Button>(R.id.btn_iniciar_sesion_login)
-        val btn_registrarse_login = findViewById<Button>(R.id.btn_registrarse_login)
+
         val til_email = findViewById<TextInputLayout>(R.id.til_email)
         val til_pass = findViewById<TextInputLayout>(R.id.til_pass)
         val sw_recordar = findViewById<Switch>(R.id.sw_recordar)
+        val btn_iniciar_sesion_login = findViewById<Button>(R.id.btn_iniciar_sesion_login)
+        val btn_registrarse_login = findViewById<Button>(R.id.btn_registrarse_login)
 
 
-
-        
+        //obtener valores de los campos de texto
         btn_iniciar_sesion_login.setOnClickListener{
-            var email=til_email.editText?.text.toString()
-            var pass=til_pass.editText?.text.toString()
-            Toast.makeText(this, email+""+pass, Toast.LENGTH_SHORT).show()
-            println("{$email} ${pass}")
+            var correo =  til_email.editText?.text.toString()
+            var clave =  til_pass.editText?.text.toString()
+            var contador=validarCampos()
+            if (contador == 0){
+                Toast.makeText( this,"Login exitoso", Toast.LENGTH_LONG).show()
+            }
+
+        }
+           // var email=til_email.editText?.text.toString()
+            //var pass=til_pass.editText?.text.toString()
+            //Toast.makeText(this, email+""+pass, Toast.LENGTH_SHORT).show()
+            //println("{$email} ${pass}")
             //val intent = Intent(this@MainActivity,UserActivity::class.java)
             //startActivity(intent)
-        }
+
         btn_registrarse_login.setOnClickListener{
             val intent = Intent(this@MainActivity,RegistrarUsuarioActivity::class.java)
             startActivity(intent)
@@ -49,36 +57,73 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun validarCampos():Int{
+        var contador:Int = 0
+        val til_email = findViewById<TextInputLayout>(R.id.til_email)
+        val til_pass = findViewById<TextInputLayout>(R.id.til_pass)
+        var correo =  til_email.editText?.text.toString()
+        var clave =  til_pass.editText?.text.toString()
+        val validate = Validate()
+
+        //validamos que los campos no esten vacios
+        if (validate.validarCampoNulo(correo)){
+            til_email.error= getString(R.string.mensaje_campo_vacio)
+            contador++
+        }
+        else{
+            //validamos que el correo sea valido
+
+            if (validate.validarFormatoCorreo(correo)){
+                til_email.error="El correo no cumple el formato usuario@correo.cl"
+                contador++
+            }
+            else{
+                til_email.error=""
+            }
+
+        }
+        if (validate.validarCampoNulo(clave)){
+            til_pass.error=getString(R.string.mensaje_campo_vacio)
+            contador++
+        }
+        else{
+            til_pass.error=""
+        }
+
+
+        return contador
+    }
+
     //Ciclo de vida de la actividad
     override fun onDestroy() {
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
         println("onDestroy()")
         super.onDestroy()
 
     }
     override fun onStart() {
         super.onStart()
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
         println("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
         println("onResume")
     }
     override fun onRestart() {
         super.onRestart()
-        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
         println("onRestart")
     }
     override fun onPause() {
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
         super.onPause()
         println("onPause()")
     }
     override fun onStop() {
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
         super.onStop()
         println("onStop()")
     }
--- app/src/main/java/com/example/appyeryherrera/RegistrarUsuarioActivity.kt
+++ app/src/main/java/com/example/appyeryherrera/RegistrarUsuarioActivity.kt
@@ -3,10 +3,12 @@ package com.example.appyeryherrera
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class RegistrarUsuarioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,28 @@ class RegistrarUsuarioActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //referencias a los widgeds
         val btn_register_user = findViewById<Button>(R.id.btn_register_user)
+        val til_correo_register = findViewById<TextInputLayout>(R.id.til_correo_register)
+        val til_user_register = findViewById<TextInputLayout>(R.id.til_user_register )
+        val til_fechanac_register = findViewById<TextInputLayout>(R.id.til_fechanac_register)
+        val til_contrasena_register = findViewById<TextInputLayout>(R.id.til_contrasena_register)
+        val til_rcontrasena_register = findViewById<TextInputLayout>(R.id.til_rcontrasena_register)
+        val sw_aceptar_terminos_register = findViewById<Switch>(R.id.sw_aceptar_terminos_register)
+
 
         btn_register_user.setOnClickListener {
+
+            var email = til_correo_register.editText?.text.toString()
+            var name = til_user_register.editText?.text.toString()
+            var pass = til_contrasena_register.editText?.text.toString()
+            var rpass = til_rcontrasena_register.editText?.text.toString()
+
+            println("${email} ${name} ${pass} ${rpass}")
+
+
             val intent = Intent(this@RegistrarUsuarioActivity, MainActivity::class.java)
             startActivity(intent)
         }
